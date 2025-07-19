feef7e334c8a00689589d170d7cdcc65c03ae945
George Appeah <appeahgeorge@gmail.com>
2025-07-19 14:05:40 +0100

    Commit to main -  Tidy the JSON object for better reading

--- api/fetch_data.py
+++ api/fetch_data.py
@@ -1,6 +1,17 @@
 # Logic to extract data from the API
 
 import requests
+import json
 response = requests.get("https://holidays.abstractapi.com/v1/?api_key=4ee75957cd434a1e8d0514606f8ec94c&country=US&year=2025&month=12&day=25")
-print(response.status_code)
+print("Status", response.status_code)
 print(response.content)
+
+print ("Data fetched successfully")
+# Make this cleaner
+
+
+data = response.json()
+print(json.dumps(data, indent=4))
+
+
+
