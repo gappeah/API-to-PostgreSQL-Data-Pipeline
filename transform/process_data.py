918e24600479268522df51c95a9a440df3acb46d
George Appeah <appeahgeorge@gmail.com>
2025-07-19 14:11:02 +0100

    Commit to main - Establish the transformation process

--- transform/process_data.py
+++ transform/process_data.py
@@ -1,1 +1,18 @@
 # Logic to clean/transform data
+
+def transform(raw_data):
+    cleaned = []
+    
+    for holiday in raw_data:
+        cleaned.append({
+            "name": holiday["name"],
+            "date_year": holiday["date_year"],
+            "date_month": holiday["date_month"],
+            "date":holiday["date"],
+            "week_day": holiday["week_day"],
+            "country": holiday["country"],
+        })
+        
+    return cleaned
+
+# I need to call this function in the main script and fix this 
